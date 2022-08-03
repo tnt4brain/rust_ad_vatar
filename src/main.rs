f74070cae156e26b3c5254beacbc9d6e75055554
Sergey Pechenko <10977752+tnt4brain@users.noreply.github.com>
2022-08-04 00:34:21 +0300

    Constants have been changed to environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use image::{imageops, GenericImageView, RgbImage};
+use image::{GenericImageView};
 use ldap3::{LdapConn, Scope, SearchEntry};
 use rocket::http::{ContentType, MediaType};
 use rocket::{response::content, State};
@@ -10,13 +10,27 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 extern crate rocket;
 extern crate core;
 
+const ERR_CONNECTION: &str = "Connection error";
+const ERR_BIND: &str = "Binding as bind_dn error";
+const ERR_SEARCH: &str = "Object search error";
+const ERR_ENCODING: &str = "Image encoding error";
+const ERR_RETRIEVAL: &str = "LDAP image retrieval error";
+
 struct HitCount {
     count: AtomicUsize,
 }
 
+struct ServerConfig {
+    ldap_uri: String,
+    bind_dn: String,
+    bind_pw: String,
+    base: String,
+    attr: String,
+}
+
 use image::imageops::FilterType;
-use rocket::http::ext::IntoCollection;
 use std::io::Cursor;
+use std::env;
 
 #[get("/")]
 fn index(hit_count: State<HitCount>) -> content::Html<String> {
@@ -25,119 +39,125 @@ fn index(hit_count: State<HitCount>) -> content::Html<String> {
 }
 
 #[get("/avatar/jpg/<email>?<size>")]
-fn avatar_jpg(email: String, size: u32) -> content::Content<Vec<u8>> {
-    let ldap_result = LdapConn::new("ldap://192.168.0.198:389");
+fn avatar_jpg(email: String, size: u32, config: State<ServerConfig>) -> content::Content<Vec<u8>> {
+    let ldap_result = LdapConn::new(&config.ldap_uri);
     let mut ldap_conn = match ldap_result {
         Ok(ldap_conn) => ldap_conn,
         Err(e) => {
             return content::Content(
                 ContentType(MediaType::HTML),
-                "Oh, error".as_bytes().to_vec(),
+                ERR_CONNECTION.as_bytes().to_vec(),
             );
         }
     };
-    let bindresult = ldap_conn.simple_bind("cn=frodo,dc=bantu,dc=ru", "secret");
+    let bindresult = ldap_conn.simple_bind(&config.bind_dn, &config.bind_pw);
     match bindresult {
         Ok(_br) => (),
         Err(e) => {
             return content::Content(
                 ContentType(MediaType::HTML),
-                "Oh, error 2".as_bytes().to_vec(),
+                ERR_BIND.as_bytes().to_vec(),
             );
         }
     }
     let searchres = match ldap_conn.search(
-        "ou=Users,dc=bantu,dc=ru",
+        &config.base,
         Scope::Subtree,
         format!("(&(objectClass=inetOrgPerson)(mail={}))", email).as_str(),
-        vec!["jpegPhoto"],
+        vec![&config.attr],
     ) {
         Ok(search) => search.success(),
         Err(_e) => {
             return content::Content(
                 ContentType(MediaType::HTML),
-                "Oh, search error".as_bytes().to_vec(),
+                ERR_SEARCH.as_bytes().to_vec(),
             );
         } /* return format!("Search error: {:?}", e), */
     };
     let (search_res, _err) = searchres.unwrap();
     if let Some(entry) = search_res.into_iter().next() {
         let z = SearchEntry::construct(entry);
-        let attr_res = z.bin_attrs.get("jpegPhoto");
+        let attr_res = z.bin_attrs.get(&config.attr);
         match attr_res {
             Some(res) => {
-                if res.len() == 1 {
-                    let arr: &[u8] = &res[0];
-                    let img = image::load_from_memory(arr);
-                    return match img {
-                        Ok(img) => {
-                            let (width, height) = img.dimensions();
-                            let ret_height = if width >= height { height } else { width };
-                            let ret_width = if width < height { width } else { height };
-                            let target_size = match (size <= 32) || (size >= 512) {
-                                false => size,
-                                true => 32,
-                            };
+                let arr: &[u8] = &res[0];
+                let img = image::load_from_memory(arr);
+                return match img {
+                    Ok(img) => {
+                        let (width, height) = img.dimensions();
+                        let ret_height = if width >= height { height } else { width };
+                        let ret_width = if width < height { width } else { height };
+                        let target_size = match (size <= 32) || (size >= 512) {
+                            false => size,
+                            true => 32,
+                        };
 
-                            let cropped_img = img.crop_imm(0, 0, ret_width, ret_height).resize(
-                                target_size,
-                                target_size,
-                                FilterType::CatmullRom,
-                            );
+                        let cropped_img = img.crop_imm(0, 0, ret_width, ret_height).resize(
+                            target_size,
+                            target_size,
+                            FilterType::CatmullRom,
+                        );
 
-                            let mut img_output: Vec<u8> = Vec::new();
-                            let write_result = cropped_img.write_to(
-                                &mut Cursor::new(&mut img_output),
-                                image::ImageOutputFormat::Jpeg(100),
-                            );
-                            match write_result {
-                                Ok(image) => {
-                                    content::Content(
-                                        ContentType(MediaType::JPEG),
-                                        img_output,
-                                    )
-                                    //    return format!("Image encoded OK");
-                                }
-                                Err(e) => {
-                                    content::Content(
-                                        ContentType(MediaType::HTML),
-                                        (format!("Encoding error {}", e).as_bytes().to_vec()),
-                                    )
-                                }
+                        let mut img_output: Vec<u8> = Vec::new();
+                        let write_result = cropped_img.write_to(
+                            &mut Cursor::new(&mut img_output),
+                            image::ImageOutputFormat::Jpeg(100),
+                        );
+                        match write_result {
+                            Ok(image) => {
+                                content::Content(
+                                    ContentType(MediaType::JPEG),
+                                    img_output,
+                                )
+                            }
+                            Err(e) => {
+                                content::Content(
+                                    ContentType(MediaType::HTML),
+                                    ERR_ENCODING.as_bytes().to_vec(),
+                                )
                             }
                         }
-                        Err(e) => {
-                            content::Content(
-                                ContentType(MediaType::HTML),
-                                (format!("Image loaded with error {}", e))
-                                    .as_bytes()
-                                    .to_vec(),
-                            )
-                            // return format!("Image loaded with error {}", e);
-                        }
-                    };
-                } else {
-                    return content::Content(
-                        ContentType(MediaType::HTML),
-                        (format!("Two or more photos, who am I to choose"))
-                            .as_bytes()
-                            .to_vec(),
-                    );
-                    // return "Two or more photos, who am I to choose".to_string();
-                }
+                    }
+                    Err(e) => {
+                        content::Content(
+                            ContentType(MediaType::HTML),
+                            ERR_RETRIEVAL.as_bytes().to_vec(),
+                        )
+                    }
+                };
             }
             None => {
-                return content::Content(
-                    ContentType(MediaType::HTML),
-                    (format!("default was here")).as_bytes().to_vec(),
-                );
+                return_default_image()
             }
         }
+    } else {
+        return_default_image()
     }
-    return content::Content(
-        ContentType(MediaType::HTML),
-        (format!("OKAY")).as_bytes().to_vec(),
-    );
+}
+
+fn return_default_image() -> content::Content<Vec<u8>> {
+    let def_img = image::open("default.png");
+
+    // let img_output =  Vec::new();
+    // let write_result = def_img.write_to(
+    //     &mut Cursor::new(&img_output),
+    //     image::ImageOutputFormat::Jpeg(100),
+    // );
+
+    match def_img {
+        Ok(image) => {
+            content::Content(
+                ContentType(MediaType::PNG),
+                image.into_bytes()
+            )
+        }
+        Err(e) => {
+            content::Content(
+                ContentType(MediaType::HTML),
+                ERR_ENCODING.as_bytes().to_vec(),
+            )
+        }
+    }
 }
 
 #[get("/count")]
@@ -146,10 +166,35 @@ fn count(hit_count: State<HitCount>) -> String {
 }
 
 fn main() {
+    let config = ServerConfig {
+        ldap_uri: match env::var("RA_LDAP_URI") {
+            Ok(val) => val,
+            Err(e) => "ldap://127.0.0.1:389".to_string()
+        },
+        bind_dn: match env::var("RA_BIND_DN") {
+            Ok(val) => val,
+            Err(e) => "cn=frodo,dc=bantu,dc=ru".to_string()
+        },
+        bind_pw: match env::var("RA_BIND_PASSWORD") {
+            Ok(val) => val,
+            Err(e) => "secret".to_string()
+        },
+        base: match env::var("RA_SEARCH_BASE") {
+            Ok(val) => val,
+            Err(e) => "ou=Users,dc=bantu,dc=ru".to_string()
+        },
+        attr: match env::var("RA_IMAGE_ATTR") {
+            Ok(val) => val,
+            Err(e) => "jpegPhoto".to_string()
+        },
+    };
+
     rocket::ignite()
         .manage(HitCount {
             count: AtomicUsize::new(0),
-        })
+        }
+        )
+        .manage(config)
         .mount("/", routes![index, avatar_jpg, count])
         .launch();
 }
